72e9b10c55657a3354f1677c245b13247f8fa5b3
Gemetalman <up201705654@fe.up.pt>
2020-12-17 11:41:19 +0000

    pus o sql no sitio certo

--- projeto/sql/bdregional.sql
+++ projeto/sql/bdregional.sql
@@ -1,37 +1,61 @@
 PRAGMA foreign_keys = ON;
 
-CREATE TABLE Utente(
-    id int PRIMARY KEY, -- AUTOINCREMENT
-    dadosClinicos text NOT NULL, --?????
-    nome text NOT NULL,
-    telefone int, --char????????????????????????
-    morada text,
-);
-
 CREATE TABLE Hospital(
-    id int PRIMARY KEY,
+    id int PRIMARY KEY AUTOINCREMENT,
     nome text NOT NULL,
     localidade text NOT NULL,
     email text UNIQUE,
 );
 
 CREATE TABLE Departamento(
-    id int PRIMARY KEY,
+    id int PRIMARY KEY AUTOINCREMENT,
     nome text NOT NULL NOT NULL,
     hospital int references Hospital NOT NULL,
 );
 
-CREATE TABLE Medico(
-    id int PRIMARY KEY,
+CREATE TABLE Pessoa(
+    id int PRIMARY KEY AUTOINCREMENT,
     nome text NOT NULL,
-    telefone int NOT NULL, --char????????????????????????
+    telefone int,  --char????????????????????????
+)
+
+CREATE TABLE Utente(
+    id int REFERENCES Pessoa PRIMARY KEY, -- AUTOINCREMENT
+    dadosClinicos text NOT NULL, --?????
+    idade int NOT NULL,
+    morada text,
+);
+
+CREATE TABLE Medico(
+    id int REFERENCES Pessoa PRIMARY KEY,
     horario text NOT NULL, --??????????????????
-    departamento int references Departamento, -- estes poderão ser null?
-    hospital int references Hospital, -- estes poderão ser null?
+    departamento int references Departamento NOT NULL, 
+    hospital int references Hospital NOT NULL,
 );
 
 CREATE TABLE Consulta( 
-    utente int references Utente PRIMARY KEY,-- tenho a vaga impressão que as primary keys deviam ser todas ids, mas não me lembro porquê
+    id int references Utente PRIMARY KEY,
     departamento int references Departamento PRIMARY KEY,
+    data text NOT NULL,  --??????????????????
+    hora text NOT NULL,  --??????????????????
+);
 
+CREATE TABLE Emergencia(
+    id int PRIMARY KEY AUTOINCREMENT,
+    hospital int references Hospital NOT NULL,
+    dataAdmissao int NOT NULL, --??????????????????
+    horaAdmissao int NOT NULL,
+    dataSaida int CHECK(dataSaida>dataAdmissao), -- check usado corretamente?
+    horaSaida int CHECK(horaSaida>horaAdmissao), -- check usado corretamente?
+);
 
+CREATE TABLE Consultorio(
+    id int PRIMARY KEY,
+    piso int NOT NULL,
+    hospital int references Hospital NOT NULL,
+);
+
+CREATE TABLE Especialidade(
+    id int PRIMARY KEY,
+    nome text NOT NULL UNIQUE, 
+);
